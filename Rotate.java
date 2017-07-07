a7b09c99dc73adbc2af26dff54541eb374d45b89
SUGUMAR SEKAR <majesticsugu@gmail.com>
2017-07-07 09:53:25 +0530

    Update Rotate.java

--- Rotate.java
+++ Rotate.java
@@ -1,6 +1,5 @@
 import java.io.*;
-import java.util.*;
-public class Rotate
+public class Git
 {
 public static void main(String args[])throws IOException
 {
@@ -8,12 +7,18 @@ BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 System.out.println("Enter the 5 digit number: ");
 Integer n = Integer.parseInt(br.readLine());
 System.out.println("Enter number of times to rotate: ");
-Integer n = Integer.parseInt(br.readLine());
-String str = a.toStringof();
+Integer k = Integer.parseInt(br.readLine());
+String str = n.toString();
 char x[] = str.toCharArray();
 for(int i=0;i<k;i++)
 {
-
+for(int j=x.length-1;j>0;j--)
+{
+	 char temp = x[j];
+     x[j] = x[j-1];
+     x[j-1] = temp;
+     System.out.print(x[j]);
+}
 }
 }
 }
